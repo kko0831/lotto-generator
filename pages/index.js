59c0037944da44bc7d509d6bc4a181bea7b4a8d0
kko0831 <kko0831@hanmail.net>
2020-02-18 00:19:46 +0900

    update up.json

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Link from 'next/link';
-import Head from 'next/head';
-import Nav from '../components/nav';
-import _ from 'underscore';
-import styled from 'styled-components';
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Nav from "../components/nav";
+import _ from "underscore";
+import styled from "styled-components";
 
 const Row = styled.div`
   display: flex;
@@ -18,54 +18,48 @@ const Number = styled.span`
   justify-content: center;
   align-items: center;
   font-size: 3rem;
-  background: ${ props => {
-    const n = parseInt( props.children, 10 );
-    if( n <= 10 ) {
-      return 'yellow;'
+  background: ${props => {
+    const n = parseInt(props.children, 10);
+    if (n <= 10) {
+      return "yellow;";
+    } else if (n <= 20) {
+      return "blue;";
+    } else if (n <= 30) {
+      return "red;";
+    } else if (n <= 40) {
+      return "black;";
+    } else {
+      return "green;";
     }
-    else if( n <= 20 ) {
-      return 'blue;'
+  }};
+  color: ${props => {
+    const n = parseInt(props.children, 10);
+    if (10 < n && n <= 20) {
+      return "white;";
+    } else if (30 < n && n <= 40) {
+      return "white;";
+    } else {
+      return "black;";
     }
-    else if( n <= 30 ) {
-      return 'red;'
-    }
-    else if( n <= 40 ) {
-      return 'black;'  
-    }
-    else {
-      return 'green;'
-    }
-  } };
-  color: ${ props => {
-    const n = parseInt( props.children, 10 );
-    if( 10 < n && n <= 20 ) {
-      return 'white;'
-    }
-    else if( 30 < n && n <= 40 ) {
-      return 'white;'
-    }
-    else {
-      return 'black;'
-    }
-  } }
+  }};
 `;
 
 function generate() {
   let numbers = [];
-  _.times( 45, n => numbers.push( n + 1 ) );
-  numbers = _.shuffle( numbers );
+  _.times(45, n => numbers.push(n + 1));
+  numbers = _.shuffle(numbers);
   numbers.length = 6;
   return numbers;
 }
 
 const Home = () => {
-  const [ list, setList ] = React.useState( [] );
+  const [list, setList] = React.useState([]);
   const regenerate = () => {
-    setList( generate() );
-  }
-  React.useEffect( () => {
+    setList(generate());
+  };
+  React.useEffect(() => {
     regenerate();
-  }, [] );
+  }, []);
   return (
     <div>
       <Head>
@@ -73,12 +67,16 @@ const Home = () => {
       </Head>
 
       <div className="container">
-          <Row>
-            { list.map( element => <Number key={ element }>{ element }</Number> ) }
-          </Row>
-          <div>
-            <button onClick={ regenerate }>재생성</button>
-          </div>
+        <Row>
+          {list.map(element => (
+            <Number key={element}>{element}</Number>
+          ))}
+        </Row>
+        <div>
+          <button style={{ fontSize: 24, marginTop: 20 }} onClick={regenerate}>
+            재생성
+          </button>
+        </div>
       </div>
 
       <style jsx>{`
@@ -93,6 +91,6 @@ const Home = () => {
       `}</style>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
